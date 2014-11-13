3059713e095af735871b41d12eb21aed2378c77d
Harry G <harrygrumbar@hotmail.com>
2014-11-13 21:31:18 +0000

    Meta Descriptions

--- templates/head.php
+++ templates/head.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php wp_title('|', true, 'right'); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/favicon.ico" />
-  <?php wp_head(); ?>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php wp_title('|', true, 'right'); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/favicon.ico" />
 
-  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
+	<?php if ($meta_desc = get_post_meta(get_the_ID(), 'meta_description', 1)) : ?>
+	<meta name="description" content="<?php echo esc_attr($meta_desc); ?>" />
+	<?php endif; ?>
+
+<?php wp_head(); ?>
+
+<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
